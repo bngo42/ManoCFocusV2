2bd140a396b9358762a81f98b92574a9d5bd9b4b
Bobi NGO <bngo@e3r5p9.42.fr>
2019-06-15 18:14:15 +0200

    Updating tweets

--- ManoCFocusV2Server/app.js
+++ ManoCFocusV2Server/app.js
@@ -1,15 +1,49 @@
 const express = require('express');
 const cors = require('cors');
+const twitter = require('node-tweet-stream');
 const app = express();
 const port = process.env.DEV_PORT || 8080;
 
 app.use(cors());
 app.listen(port);
 
+const search_limit = 20;
+let search_count = 0;
+let tweets = new twitter({
+    consumer_key: '2g2I3k9IigJPDW2fSg6gDGagw',
+    consumer_secret: '8WgDHbcfk3NVWiXtJFdHEnekqYSRdE55VCC5qfGo2DgRPdCQzA',
+    token: '62844479-4KAdVjdocQlah9RklJDltoKNQ7EgjcnkpQxRU7qhu',
+    token_secret: 'Fje4ipPlxCZX28Oi0moqOI3JZIb4wFP4RUDR6ZILBpn4m'
+});
+
+const trackedWord = 'sah';
+
+tweets.untrackAll();
+tweets.language('fr');
+tweets.track(trackedWord);
+tweets.on('tweet', tweet => {
+    search_count++;
+    if (search_count >= search_limit){
+        tweets.untrack(trackedWord);
+        console.log(`Stop tracking ${trackedWord}`);
+        timeout(10000).then(() => {
+            tweets.track(trackedWord);
+            search_count = 0;
+            console.log(`Started tracking ${trackedWord}`);
+        });
+    }
+    if (!tweet.retweeted_status){
+        console.log(`@${tweet.user.screen_name}:\n${tweet.text}`);
+    }
+});
+
 //Global Parameters
 let showTweets = false;
 let showMessage = {state: false, msg: "", duration: 1000};
 let showCard = false;
+
+
+
 
 
 app.get('/', (req, res) => {
