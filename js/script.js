26e622f2d3437d336f8367578222bf16214ac023
Bobi NGO <bngo@e3r10p7.42.fr>
2019-06-29 17:12:57 +0200

    Update

--- js/script.js
+++ js/script.js
@@ -1,23 +1,45 @@
+let navbarTop = document.querySelector('.navbar-top');
 let parallaxer = document.querySelector('.parallaxer');
 let items = parallaxer.querySelectorAll('.item');
-let offset = 100;
 let scroll = window.pageYOffset;
+
+
+
+// ----------------------- MAIN -----------------------
+    console.log(navbarTop);
+    updateNavBar();
+// ----------------------------------------------------
+
 
 items.forEach(item => {
     if (!item.classList.contains('floating')){
         item.classList.add('floating');
     }
-    item.style.background = `url('${item.dataset.src}')`;
+    if (item.dataset.src){
+        item.style.background = `url('${item.dataset.src}')`;
+    }
     item.style.backgroundRepeat = 'no-repeat';
     item.style.backgroundSize = 'contain';
-    item.style.backgroundPosition = 'center';
-    item.style.top = `${((scroll - offset) * item.dataset.ratio)}px`;
+    item.style.backgroundPosition = 'bottom';
+    item.style.top = `${((scroll - item.dataset.offset) * item.dataset.ratio)}px`;
 });
 
 
 window.addEventListener('scroll', e => {
     scroll = window.pageYOffset;
     items.forEach(item => {
-        item.style.top = `${((scroll - offset) * item.dataset.ratio)}px`;
+        item.style.top = `${((scroll - item.dataset.offset) * item.dataset.ratio)}px`;
     });
+
+   updateNavBar();
 });
+
+function updateNavBar() {
+    if (scroll > 80) {
+        if (!navbarTop.classList.contains('fixed'))
+            navbarTop.classList.add('fixed');
+    } else {
+        if (navbarTop.classList.contains('fixed'))
+            navbarTop.classList.remove('fixed');
+    }
+}
