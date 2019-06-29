8de18a1f423d4242615fa23274dd10a22bf51261
Bobi NGO <bngo@e3r10p7.42.fr>
2019-06-29 11:04:09 +0200

    Restart

---
+++ js/script.js
@@ -0,0 +1,23 @@
+let parallaxer = document.querySelector('.parallaxer');
+let items = parallaxer.querySelectorAll('.item');
+let offset = 100;
+let scroll = window.pageYOffset;
+
+items.forEach(item => {
+    if (!item.classList.contains('floating')){
+        item.classList.add('floating');
+    }
+    item.style.background = `url('${item.dataset.src}')`;
+    item.style.backgroundRepeat = 'no-repeat';
+    item.style.backgroundSize = 'cover';
+    item.style.backgroundPosition = 'center';
+    item.style.top = `${((scroll - offset) * item.dataset.ratio)}px`;
+});
+
+
+window.addEventListener('scroll', e => {
+    scroll = window.pageYOffset;
+    items.forEach(item => {
+        item.style.top = `${(-(scroll - offset) * item.dataset.ratio)}px`;
+    });
+});
