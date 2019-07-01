08ddf7e62a06214010a3738e378b99fe2b73e294
Bobi NGO <bngo@e1r3p10.42.fr>
2019-07-01 15:28:46 +0200

    Update

--- js/script.js
+++ js/script.js
@@ -7,23 +7,23 @@ let scroll = window.pageYOffset;
     updateNavBar();
 // ----------------------------------------------------
 
-
-items.forEach(item => {
-    if (!item.classList.contains('floating')){
-        item.classList.add('floating');
-    }
-    if (item.dataset.src){
-        item.style.background = `url('${item.dataset.src}')`;
-    }
-    item.style.backgroundRepeat = 'no-repeat';
-    item.style.backgroundSize = 'cover';
-    item.style.backgroundPosition = 'bottom';
-    item.style.top = `${((scroll - item.dataset.offset) * item.dataset.ratio)}px`;
-});
-
-
-window.addEventListener('scroll', updateScroll);
-window.addEventListener('touchmove', updateScroll);
+if (!isMobile()) {   
+    items.forEach(item => {
+        if (!item.classList.contains('floating')){
+            item.classList.add('floating');
+        }
+        if (item.dataset.src){
+            item.style.background = `url('${item.dataset.src}')`;
+        }
+        item.style.backgroundRepeat = 'no-repeat';
+        item.style.backgroundSize = 'cover';
+        item.style.backgroundPosition = 'bottom';
+        item.style.top = `${((scroll - item.dataset.offset) * item.dataset.ratio)}px`;
+    });
+   
+    window.addEventListener('scroll', updateScroll);
+    window.addEventListener('touchmove', updateScroll);
+}
 
 function updateScroll() {
     scroll = window.pageYOffset;
@@ -34,4 +34,16 @@ function updateScroll() {
     }
         
    updateNavBar();
+}
+
+function isMobile() {
+    return (
+        navigator.userAgent.match(/Android/i)
+    ||  navigator.userAgent.match(/webOS/i)
+    ||  navigator.userAgent.match(/iPhone/i)
+    ||  navigator.userAgent.match(/iPad/i)
+    ||  navigator.userAgent.match(/iPod/i)
+    ||  navigator.userAgent.match(/BlackBerry/i)
+    ||  navigator.userAgent.match(/Windows Phone/i)
+    );
 }
