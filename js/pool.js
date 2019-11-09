7af49c19e7faea124c979564baad0f2d24403e57
bngo42 <bngo@student.42.fr>
2019-11-09 01:02:06 +0100

    link progression bar to total donation

---
+++ js/pool.js
@@ -0,0 +1,36 @@
+let barFill = document.querySelector('.bar-fill'),
+    poolLabel = document.querySelector('.progress-current');
+
+
+updateProgress();
+setInterval(updateProgress, 1000);
+
+
+function updateProgress() {   
+    getCurrentTotal().then(val => {
+        let percentage = calculatePercent(20000, val.current_value);
+        setBarPercent(percentage);
+        setCurrentLabel(val.current_value);
+    }).catch(console.log);
+}
+
+function getCurrentTotal() {
+    return new Promise((resolve, reject) => {
+        fetch('https://tweets-server.herokuapp.com/donations')
+            .then(res => res.json())
+            .then(resolve)
+            .catch(reject);
+    });
+}
+
+function calculatePercent(total, current) {
+    return (current * 100) / total;
+}
+
+function setBarPercent(percent) {
+    barFill.style.width = percent + "%";
+}
+
+function setCurrentLabel(val) {
+    poolLabel.innerHTML = val + '€';
+}
